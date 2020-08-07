95803f4c6d2ebae47b943400f70e4f43971f3074
Michael Gill <mike@cloudgalacticos.co.uk>
2020-08-07 15:05:12 +0100

    final updates

--- 1_setupMyDemo.sh
+++ 1_setupMyDemo.sh
@@ -4,8 +4,12 @@
 
 set -e
 
+rm -rf force-app/main/default/objects
+rm -rf force-app/main/default/permissionsets
+rm -rf force-app/main/default/tabs
+
 echo "Creating scratch org against a devhub org with alais devhub0 (change to own)"
-sfdx force:org:create -f config/project-scratch-def.json -s -a demo -d 30 -v devhub0 -w 10
+sfdx force:org:create -f config/project-scratch-def.json -s -a demo -d 1 -v devhub0 -w 10
 
 echo "Creating a Demo Billing Custom Oject in Salesforce..."
 sfdx shane:object:create -a billing_usage__c -l "Billing Usage" -p "Billing Usage" -t custom --autonumberformat={0000} --enterprise --nametype=AutoNumber
@@ -13,7 +17,7 @@ sfdx shane:object:create -a billing_usage__c -l "Billing Usage" -p "Billing Usag
 echo "There is my custom object, now adding a custom field..."
 sfdx shane:object:field -a ExternalId__c -l 255 -n "External Id" -t Text -o billing_usage__c
 sfdx force:source:push
-echo "That is in my org now, how cool is that?"
+echo "That is in your org now, how cool is that?"
 sleep 10
 
 sfdx shane:object:tab -o billing_usage__c -i 18
@@ -23,10 +27,9 @@ sleep 10
 echo "What about permissions?, no problem... hold on one sec"
 
 sfdx shane:permset:create -n Access_Billing_Usage -o billing_usage__c -t
-sfdx shane:permset:create -o billing_usage__c -f ExternalId__c -n Access_Billing_Usage
 sfdx force:source:push
 sfdx force:user:permset:assign -n Access_Billing_Usage
-echo "All done, oh and we assigned it to you as well!"
+echo "All done, oh and we assigned to you as well!"
 sleep 10
 
 echo "Ready for the next trick?"
--- 2_uploadFile.sh
+++ 2_uploadFile.sh
@@ -4,8 +4,8 @@ dxoutput=$(sfdx force:data:record:create -s Billing_Usage__c -v "ExternalId__c=1
 recordId=$(echo $dxoutput | cut -c30-47)
 echo $recordId
 sfdx shane:data:file:upload -f data/billing-usage.csv -p $recordId
-sed 's/123456789012345678/${recordId}/g' 3_readFile.apex > 4_outputFile.apex
-sfdx force:apex:execute -f outputFile.apex > 5_output.log
+sed "s/123456789012345678/${recordId}/g" 3_readFile.apex > 4_outputFile.apex
+sfdx force:apex:execute -f 4_outputFile.apex > 5_output.log
 
 echo "There you go - let's open Salesforce and check the file is actually there!"
 sleep 15
