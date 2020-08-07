f3b1a65d5adcea8faab1c31c8d47648f59052a1f
Michael Gill <mike@cloudgalacticos.co.uk>
2020-08-07 14:45:19 +0100

    updated scripts

--- 1_setupMyDemo.sh
+++ 1_setupMyDemo.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
+# Some demo showing creating demo scratch org automatically, uploading and parsing a CSV in apex!!!
+
 set -e
 
+echo "Creating scratch org against a devhub org with alais devhub0 (change to own)"
 sfdx force:org:create -f config/project-scratch-def.json -s -a demo -d 30 -v devhub0 -w 10
 
-echo "Create a Azure Billing Oject in salesforce..."
+echo "Creating a Demo Billing Custom Oject in Salesforce..."
 sfdx shane:object:create -a billing_usage__c -l "Billing Usage" -p "Billing Usage" -t custom --autonumberformat={0000} --enterprise --nametype=AutoNumber
+
+echo "There is my custom object, now adding a custom field..."
+sfdx shane:object:field -a ExternalId__c -l 255 -n "External Id" -t Text -o billing_usage__c
 sfdx force:source:push
 echo "That is in my org now, how cool is that?"
-echo "Now adding a field..."
-sfdx shane:object:field -a ExternalId__c -l 255 -n "External Id" -t Text -o billing_usage__c
-
+sleep 10
 
 sfdx shane:object:tab -o billing_usage__c -i 18
 sfdx force:source:push
-echo "There is my tab, are you impressed yet?"
-
-echo "What about permissions, no problem... hold on one sec"
+echo "Forgot the tab, no problem..."
+sleep 10
+echo "What about permissions?, no problem... hold on one sec"
 
 sfdx shane:permset:create -n Access_Billing_Usage -o billing_usage__c -t
 sfdx shane:permset:create -o billing_usage__c -f ExternalId__c -n Access_Billing_Usage
 sfdx force:source:push
 sfdx force:user:permset:assign -n Access_Billing_Usage
+echo "All done, oh and we assigned it to you as well!"
+sleep 10
 
-echo "Done, oh and we assigned it to you!
-
+echo "Ready for the next trick?"
+sleep 10
+bash 2_uploadFile.sh
 
 
 
--- 2_uploadFile.sh
+++ 2_uploadFile.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
+set -e
 dxoutput=$(sfdx force:data:record:create -s Billing_Usage__c -v "ExternalId__c=123")
 recordId=$(echo $dxoutput | cut -c30-47)
 echo $recordId
 sfdx shane:data:file:upload -f data/billing-usage.csv -p $recordId
 sed 's/123456789012345678/${recordId}/g' 3_readFile.apex > 4_outputFile.apex
 sfdx force:apex:execute -f outputFile.apex > 5_output.log
+
+echo "There you go - let's open Salesforce and check the file is actually there!"
+sleep 15
+sfdx force:org:open
